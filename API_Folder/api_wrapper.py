877bd6a789532343bf38d48b69d3a21e85913734
lukepark2004 <lukepark2004@gmail.com>
2022-07-11 23:24:54 -0700

    Trying IBapi commands out.

--- API_Folder/api_wrapper.py
+++ API_Folder/api_wrapper.py
@@ -2,10 +2,20 @@
 Has the functions necessary to start communication with the api
 """
 
-"""
-from ib_insync import *
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper  
 
-ib= IB()
-ib.connect()
-"""
+class IBapi(EWrapper, EClient):
+     def __init__(self):
+         EClient.__init__(self, self) 
 
+app = IBapi()
+app.connect('192.168.1.28', 7947, 1230)
+app.run()
+
+
+#Uncomment this section if unable to connect
+#and to prevent errors on a reconnect
+import time
+time.sleep(5)
+app.disconnect()
