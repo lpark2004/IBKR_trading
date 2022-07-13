7e5554af6f5ccf35e563085bf8fb346f00c450c2
lukepark2004 <lukepark2004@gmail.com>
2022-07-12 21:26:53 -0700

    Daily_Manager V1.0 ready to go

--- Automated_Capacities/daily_manager.py
+++ Automated_Capacities/daily_manager.py
@@ -9,43 +9,14 @@ import datetime
 import pytz
 import pause
 import pickle 
-
-nyse = mcal.get_calendar('NYSE')
-#data_path = "C:\Users\Luke Park\Documents\IBKR_trading\data"
-
-#time right now in New York
-now = datetime.datetime.now(pytz.timezone('US/Eastern'))
-date_today = now.date()
-hour = now.hour
-minute = now.minute
-date_next_week = date_today + datetime.timedelta(days = 7)
-
-#time difference between datetime default and Eastern time
-time_dif = 3
+import os
+import sys
 
 
-open_day = nyse.valid_days(start_date = str(date_today), end_date = str(date_today))
 
-#if today is not a valid training day, or it is past market open time, wait until next trading day
-if len(open_day) == 0 or hour > 6 or (minute >= 30 and hour == 6):
-    next_trading_day = nyse.valid_days(start_date = str(date_today), end_date = str(date_next_week))[0]
-    ntd = next_trading_day
-    next_trading_day = datetime.datetime(ntd.year, ntd.month, ntd.day, 9 - time_dif, 20)
-    pause.until(next_trading_day)
-    
-
-
-#if today is a valid training day and market has not opened yet
-elif len(open_day) != 0 and (hour == 6 and minute < 30):
-    file_name = str(now.strftime('%d%b%Y')) + ".pickle"
-
-file_name = str(now.strftime('%d%b%Y')) + ".pickle"
-print(file_name)
-
-
-"""
 while True:
     nyse = mcal.get_calendar('NYSE')
+    data_path = "C:/Users/Luke Park/Documents/IBKR_trading/data/"
 
     #time right now in New York
     now = datetime.datetime.now(pytz.timezone('US/Eastern'))
@@ -59,18 +30,30 @@ while True:
 
 
     open_day = nyse.valid_days(start_date = str(date_today), end_date = str(date_today))
+    next_trading_week = nyse.valid_days(start_date = str(date_today), end_date = str(date_next_week))
+
 
     #if today is not a valid training day, or it is past market open time, wait until next trading day
     if len(open_day) == 0 or hour > 6 or (minute >= 30 and hour == 6):
-        next_trading_day = nyse.valid_days(start_date = str(date_today), end_date = str(date_next_week))[0]
+        next_trading_day = next_trading_week[0]
         ntd = next_trading_day
         next_trading_day = datetime.datetime(ntd.year, ntd.month, ntd.day, 9 - time_dif, 20)
         pause.until(next_trading_day)
         continue
 
     #if today is a valid training day and market has not opened yet
-    #elif len(open_day) != 0 and (hour == 6 and minute < 30):
-"""
+    elif len(open_day) != 0 and (hour == 6 and minute < 30):
+        pause.until(now.year, now.month, now.day, 16 - time_dif, 10)
+        file_name = data_path + str(now.strftime('%d%b%Y')) + ".pickle"
+        outfile = open(file_name, 'wb')
+        pickle.dump("Hello World", outfile)
+        outfile.close
+        next_trading_day = next_trading_week[1]
+        ntd = next_trading_day
+        next_trading_day = datetime.datetime(ntd.year, ntd.month, ntd.day, 9 - time_dif, 20)
+        pause.until(next_trading_day)
+        continue
+
 
 
 
---
+++ Utilities/general_utilities.py
@@ -0,0 +1,15 @@
+"""
+general utility functions needed throughout the scope of this project
+"""
+import pickle
+
+#returns everything inside of a pickle file
+def file_opener(file):
+    '''
+    opens pickle files
+    '''
+    new_file = open(file, "rb")
+    objects = []
+    objects.append(pickle.load(new_file))
+
+    return objects
